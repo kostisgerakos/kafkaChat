7cb33f3d40f4f1f63d35bac49c6b3070a4c6a7ae
kgerakos <kgerakos@di.uoa.gr>
2018-05-20 15:58:03 +0300

    group chat clean

--- src/main/java/com/kafka/chat/graphics/ChatMain.java
+++ src/main/java/com/kafka/chat/graphics/ChatMain.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionListener;
 
 public class ChatMain {
 
-	String username = "kostis";
+	String username = "fwkiwn";
 	String topic;
 	JTextField messageBox;
 	JTextArea chatBox;
--- src/main/java/com/kafka/chat/graphics/TopicsList.java
+++ src/main/java/com/kafka/chat/graphics/TopicsList.java
@@ -1,46 +1,35 @@
 package com.kafka.chat.graphics;
 
-
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Set;
 
 import javax.swing.*;
 
-import org.apache.kafka.common.PartitionInfo;
-
 import com.kafka.chat.consumers.ListTopicsConsumer;
 
 public class TopicsList {
 	final ListTopicsConsumer group;
 
-	
 	public TopicsList() {
-		this.group = new ListTopicsConsumer("eagle5.di.uoa.gr:9092","http://eagle5.di.uoa.gr:8081");
+		this.group = new ListTopicsConsumer("eagle5.di.uoa.gr:9092", "http://eagle5.di.uoa.gr:8081");
 		display(group.getGroupTopics());
 	}
 
-	
-
-	
 	public void display(Set<String> topics) {
 		final JList list = new JList(topics.toArray());
 		final JScrollPane pane = new JScrollPane(list);
 		final JFrame frame = new JFrame("Chat Groups");
-
-		// create a button and add action listener
 		final JButton btnGet = new JButton("Get Selected");
 		btnGet.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//String selectedElem = "";
 				int selectedIndices[] = list.getSelectedIndices();
 				for (int j = 0; j < selectedIndices.length; j++) {
 					String elem = (String) list.getModel().getElementAt(selectedIndices[j]);
-					//frame.setVisible(false);
-					ChatMain chat = new ChatMain(elem);	
+					// frame.setVisible(false);
+					ChatMain chat = new ChatMain(elem);
 				}
-				//JOptionPane.showMessageDialog(frame, "You've selected:" + selectedElem);
-			}// end actionPerformed
+			}
 		});
 
 		frame.setLayout(new BorderLayout());
